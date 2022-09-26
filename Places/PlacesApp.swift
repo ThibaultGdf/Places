ba2a1a70a079f1ddee5faa30d3348a0652dc459c
ThibaultGdf <thibaultgodef81@gmail.com>
2022-09-26 09:54:46 +0200

    Initial Commit

---
+++ Places/PlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesApp.swift
+//  Places
+//
+//  Created by Thibault GODEFROY on 26/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct PlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
